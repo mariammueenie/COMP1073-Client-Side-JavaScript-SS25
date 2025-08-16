d1bed82bec2bbfc9762de2bf08fc75b9d2217d25
mariammueenie <156461593+mariammueenie@users.noreply.github.com>
2025-08-15 20:51:58 -0400

    Loose plan for JS file

---
+++ Assignment2/js/index.js
@@ -0,0 +1,28 @@
+
+
+// plan:
+
+// Define smoothie class
+
+// Define ingredients as properties of the class 
+
+// Restrict number of ingredients that can be selected
+
+// Create method to display smoothie ingredients
+    // return all ingredients selected
+    // need to return each property of smoothie class
+    // need to return "none" if no ingredient selected
+
+// Create object to hold user selections
+
+// Add event listener to form submission
+
+// Render smoothie with user selections and display
+
+
+
+
+
+
+
+
